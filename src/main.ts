32851090dfc4ba54c2821a8db854e8b5f971a81e
sb2bg <sullivanbognar@gmail.com>
2022-02-04 16:36:40 -0600

    Add test express route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
 
 const prisma = new PrismaClient();
 
-const main = async () => {};
+const main = async () => {
+  const app = express();
+
+  app.get("/", async (_req, res) => {
+    res.json({
+      hello: "world",
+    });
+  });
+};
 
 main()
   .catch((err) => {
