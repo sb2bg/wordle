b4420124ee52c4a070f9b40743c76442cbc590a4
sb2bg <sullivanbognar@gmail.com>
2022-02-05 14:27:10 -0600

    Extract route logic to seperate file

---
+++ src/createUser.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from ".prisma/client";
+import { Request, Response } from "express";
+import argon2 from "argon2";
+import { verifyPassword, verifyUsername, verifyEmail } from "./verify";
+
+export const createUser = async (
+  req: Request,
+  res: Response,
+  prisma: PrismaClient
+) => {
+  let { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400).json({
+      error: "username, email, and password are required",
+    });
+  }
+
+  username = username.trim().toLowerCase();
+  email = email.trim();
+
+  const usernameVerification = await verifyUsername(username, prisma);
+
+  if (!usernameVerification.valid) {
+    res.status(400).json({
+      error: usernameVerification.message,
+    });
+  }
+
+  const emailVerification = await verifyEmail(email, prisma);
+
+  if (!emailVerification.valid) {
+    res.status(400).json({
+      error: emailVerification.message,
+    });
+  }
+
+  const passwordVerification = verifyPassword(password);
+
+  if (!passwordVerification.valid) {
+    res.status(400).json({
+      error: passwordVerification.message,
+    });
+  }
+
+  const user = await prisma.user.create({
+    data: { username, email, password: await argon2.hash(password) },
+  });
+
+  res.json(user);
+};
