fa261e2f51e97462668ea3038bed3206d38b9c76
sb2bg <sullivanbognar@gmail.com>
2022-02-06 15:26:48 -0600

    Replace verifyToken with authToken middlewear

--- src/jwt.ts
+++ src/jwt.ts
@@ -1,4 +1,6 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response } from "express";
+import { GenericResponse } from "./types/genericResponse";
 
 const secret = process.env.TOKEN_SECRET;
 
@@ -12,6 +14,30 @@ export const generateToken = (userId: number): string => {
   });
 };
 
-export const verifyToken = (token: string): string | JwtPayload => {
-  return jwt.verify(token, secret);
+export const authToken = (
+  req: Request,
+  res: Response<GenericResponse>,
+  next: Function
+) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({
+      success: false,
+      message: "No token provided",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err || !decoded) {
+      res.status(401).json({
+        success: false,
+        message: "Token is not valid or has expired",
+      });
+    }
+
+    req.userId = (decoded as JwtPayload).userId;
+    next();
+  });
 };
