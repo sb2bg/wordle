1bb17ded9a39c0fa61f0bc6fb1a328d095f11786
sb2bg <sullivanbognar@gmail.com>
2022-02-06 15:26:11 -0600

    add user jwt authorization

--- src/main.ts
+++ src/main.ts
@@ -5,31 +5,25 @@ import { getDailyWord } from "./routes/getDailyWord";
 import { loginUser } from "./routes/loginUser";
 import { solveWord } from "./routes/solveWord";
 import dotenv from "dotenv";
+import { authToken } from "./jwt";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-const prisma = new PrismaClient();
+
+export const prisma = new PrismaClient();
 
 const main = async () => {
   const app = express();
 
   app.use(express.json());
 
-  app.post("/users", async (req, res) => {
-    await createUser(req, res, prisma);
-  });
+  app.post("/users", createUser);
 
-  app.post("/login", async (req, res) => {
-    await loginUser(req, res, prisma);
-  });
+  app.post("/login", loginUser);
 
-  app.post("/solve", async (req, res) => {
-    await solveWord(req, res, prisma);
-  });
+  app.post("/solve", authToken, solveWord);
 
-  app.get("/daily", async (_req, res) => {
-    await getDailyWord(res, prisma);
-  });
+  app.get("/daily", authToken, getDailyWord);
 
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
