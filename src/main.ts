2bf92ef6a0acdf6614baffc6ff914d91f46ab5a1
sb2bg <sullivanbognar@gmail.com>
2022-02-06 16:34:00 -0600

    Add new routes

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { loginUser } from "./routes/loginUser";
 import { solveWord } from "./routes/solveWord";
 import dotenv from "dotenv";
 import { authToken } from "./jwt";
+import { changeUsername } from "./routes/changeUsername";
+import { changePassword } from "./routes/changePassword";
+import { deleteUser } from "./routes/deleteUser";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -17,12 +20,12 @@ const main = async () => {
 
   app.use(express.json());
 
-  app.post("/users", createUser);
-
+  app.post("/create-user", createUser);
   app.post("/login", loginUser);
-
   app.post("/solve", authToken, solveWord);
-
+  app.post("/change-username", authToken, changeUsername);
+  app.post("/change-password", authToken, changePassword);
+  app.post("/delete-user", authToken, deleteUser);
   app.get("/daily", authToken, getDailyWord);
 
   app.listen(PORT, () => {
